3dfee03e1450424bf4cfe4fcce45ca7c40c28f7d
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2021-06-23 18:41:28 +0530

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,7 +131,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        SID IS AMAZING
+        SID AND JULIEN ARE AMAZING
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
